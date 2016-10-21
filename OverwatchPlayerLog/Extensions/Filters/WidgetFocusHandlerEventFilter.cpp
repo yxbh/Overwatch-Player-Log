8f57e1b38dd6d3480b0ad361982c665d62a18707
Benjamin Huang <y.x.ben.h@gmail.com>
2016-10-21 18:14:56 +1100

    improve focus handling.

--- OverwatchPlayerLog/Extensions/Filters/WidgetFocusHandlerEventFilter.cpp
+++ OverwatchPlayerLog/Extensions/Filters/WidgetFocusHandlerEventFilter.cpp
@@ -1,6 +1,7 @@
 #include "WidgetFocusHandlerEventFilter.hpp"
 #include <QDebug>
 #include <QApplication>
+#include <QMouseEvent>
 #include <QWidget>
 #include "Helpers/QEventNameHelper.hpp"
 
@@ -15,19 +16,21 @@ bool WidgetFocusHandlerEventFilter::eventFilter(QObject *watched, QEvent *event)
         return false;
 
     auto focusWidget = qApp->focusWidget();
-    if (nullptr != focusWidget && watched != focusWidget)
+    if (nullptr == focusWidget || watched == focusWidget)
+        return false;
+
+    if (event->type() == QEvent::MouseButtonPress ||
+        event->type() == QEvent::MouseButtonRelease ||
+        event->type() == QEvent::MouseButtonDblClick)
     {
-        if (event->type() == QEvent::MouseButtonPress ||
-            event->type() == QEvent::MouseButtonRelease ||
-            event->type() == QEvent::MouseButtonDblClick)
+        auto focusWidgetClassName = focusWidget->metaObject()->className();
+        if (focusWidgetClassName == QString("QLineEdit") ||
+            focusWidgetClassName == QString("QPlainTextEdit") ||
+            focusWidgetClassName == QString("QListView"))
         {
-            auto focusWidgetClassName = focusWidget->metaObject()->className();
-            if (focusWidgetClassName == QString("QLineEdit") ||
-                focusWidgetClassName == QString("QPlainTextEdit") ||
-                focusWidgetClassName == QString("QListView"))
-            {
+            auto mouseEvent = static_cast<const QMouseEvent*>(event);
+            if (!focusWidget->rect().contains(focusWidget->mapFromGlobal(mouseEvent->globalPos())))
                 focusWidget->clearFocus();
-            }
         }
     }
 
