5bbf74308ca21d9cf513aafabaf31ae45d9fe1f3
Benjamin <y.x.ben.h+github@gmail.com>
2016-10-28 21:06:22 +1100

    fix indentation

--- OverwatchPlayerLog/appinfo-hpp-gen.sh
+++ OverwatchPlayerLog/appinfo-hpp-gen.sh
@@ -4,19 +4,19 @@
 
 cat <<EOT > appinfo.hpp
 #pragma once
-#define APP_INFO_COMPANY "%~1"
-#define APP_INFO_PRODUCT "%~2"
+#define APP_INFO_COMPANY     "%~1"
+#define APP_INFO_PRODUCT     "%~2"
 #define APP_INFO_DESCRIPTION "%~3"
-#define APP_INFO_COPYRIGHT "%~4"
+#define APP_INFO_COPYRIGHT   "%~4"
 
 namespace app
 {
 namespace info
 {
-static const char* const COMPANY = "%~1";
-static const char* const PRODUCT = "%~2";
+static const char* const COMPANY     = "%~1";
+static const char* const PRODUCT     = "%~2";
 static const char* const DESCRIPTION = "%~3";
-static const char* const COPYRIGHT = "%~4";
+static const char* const COPYRIGHT   = "%~4";
 }
 } 
 EOT
