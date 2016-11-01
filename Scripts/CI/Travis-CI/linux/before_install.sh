d1a2fc99fd0c527b53e4f30c716fb905f18e08b7
fraefaer <aefr@awefawe.com>
2016-11-01 13:40:27 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/before_install.sh
+++ Scripts/CI/Travis-CI/linux/before_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -ev
 
-sudo add-apt-repository ppa:beineri/opt-qt57-trusty -y;
+# sudo add-apt-repository ppa:beineri/opt-qt57-trusty -y;
+# sudo apt-get update -qq
+
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
--- Scripts/CI/Travis-CI/linux/install.sh
+++ Scripts/CI/Travis-CI/linux/install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ev
 
-sudo apt-get install -qq qt57base
+# sudo apt-get install -qq qt57base
 
-echo '#### begin script content ####'
-cat /opt/qt57/bin/qt57-env.sh
-echo '#### end script content ####'
+# echo '#### begin script content ####'
+# cat /opt/qt57/bin/qt57-env.sh
+# echo '#### end script content ####'
 
-echo $0
-sh /opt/qt57/bin/qt57-env.sh
-# echo 'source /opt/qt57/bin/qt57-env.sh'
+# source /opt/qt57/bin/qt57-env.sh
+
+sudo apt-get install qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev
+sudo apt-get install qt5-default qttools5-dev-tools
