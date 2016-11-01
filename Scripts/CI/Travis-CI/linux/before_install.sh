8fe1da1bcf67f7f8180b2ba07da3438a0a590b83
fraefaer <aefr@awefawe.com>
2016-11-01 14:17:14 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/before_install.sh
+++ Scripts/CI/Travis-CI/linux/before_install.sh
@@ -4,12 +4,12 @@ set -ev
 sudo add-apt-repository ppa:beineri/opt-qt57-trusty -y;
 sudo apt-get update -qq
 
-# # install newer compiler
-# sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-# sudo apt-get update
-# sudo apt-get install gcc-6 g++-6
-# # set as default
-# sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+# install newer compiler
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update
+sudo apt-get install gcc-6 g++-6
+# set as default
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 # # add repo for Qt
 # sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa -y
