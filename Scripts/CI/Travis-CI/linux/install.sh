909ed986e388d921205d6af3ede2311cc38e0844
fraefaer <aefr@awefawe.com>
2016-11-01 14:23:36 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/install.sh
+++ Scripts/CI/Travis-CI/linux/install.sh
@@ -1,11 +1,5 @@
-#!/bin/bash
-# set -ev
 
 sudo apt-get install -qq qt57base
-
-echo '#### begin script content ####'
-cat /opt/qt57/bin/qt57-env.sh
-echo '#### end script content ####'
 
 source /opt/qt57/bin/qt57-env.sh
 echo $PATH
