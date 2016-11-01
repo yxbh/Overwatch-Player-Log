1baf07049ef66c019657e34a74e528bbcb596d03
fraefaer <aefr@awefawe.com>
2016-11-01 14:19:45 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/install.sh
+++ Scripts/CI/Travis-CI/linux/install.sh
@@ -7,7 +7,8 @@ echo '#### begin script content ####'
 cat /opt/qt57/bin/qt57-env.sh
 echo '#### end script content ####'
 
-# source /opt/qt57/bin/qt57-env.sh
+source /opt/qt57/bin/qt57-env.sh
+echo $PATH
 
 # sudo apt-get install qtbase5-dev libsqlite3-dev libqt5svg5-dev
 # sudo apt-get install qt5-default qttools5-dev-tools
