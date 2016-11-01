4b34f81f2c97c0aba37ca1eac882a2da1cc9bfc4
Benjamin <y.x.ben.h+github@gmail.com>
2016-11-01 19:09:23 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/install.sh
+++ Scripts/CI/Travis-CI/linux/install.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-set -ev
+##!/bin/bash
+#set -ev
 
 sudo apt-get install -qq qt57base
 
 source /opt/qt57/bin/qt57-env.sh
-echo $PATH
-
-# sudo apt-get install qtbase5-dev libsqlite3-dev libqt5svg5-dev
-# sudo apt-get install qt5-default qttools5-dev-tools
