40a5eda4730e28c7feb8cfe800f06116b6a54d87
fraefaer <aefr@awefawe.com>
2016-11-01 15:30:57 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/before_install.sh
+++ Scripts/CI/Travis-CI/linux/before_install.sh
@@ -6,7 +6,7 @@ sudo apt-get update -qq
 
 # install newer compiler
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
+sudo apt-get update -qq
 sudo apt-get install gcc-6 g++-6
 # set as default
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
