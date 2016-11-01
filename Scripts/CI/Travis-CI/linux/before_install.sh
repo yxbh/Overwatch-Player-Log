268c9840c81aea6be836cf3f1d7913b5fdef347f
fraefaer <aefr@awefawe.com>
2016-11-01 13:57:19 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/before_install.sh
+++ Scripts/CI/Travis-CI/linux/before_install.sh
@@ -5,12 +5,12 @@ set -ev
 # sudo apt-get update -qq
 
 # install newer compiler
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install gcc-6 g++-6
 # set as default
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
 
 # add repo for Qt
-sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa -y
 sudo apt-get update -qq
