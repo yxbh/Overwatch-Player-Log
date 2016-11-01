a5e4e86ef99a22b59e47da9cc1aee9e8b41912b8
Benjamin <y.x.ben.h+github@gmail.com>
2016-11-01 19:42:50 +1100

    WIP Travis-CI for osx

--- Scripts/CI/Travis-CI/osx/before_install.sh
+++ Scripts/CI/Travis-CI/osx/before_install.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
-set -ev
+# #!/bin/bash
+# set -ev
 
-# Prepare build environment
-mkdir dependencies
+# # Prepare build environment
+# mkdir dependencies
 
 # Update homebrew
 brew update
+# Update homebrew software
+brew upgrade
