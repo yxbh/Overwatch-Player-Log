ddaea91db0b69b91db8b7f6680f4e8abf25f42d9
fraefaer <aefr@awefawe.com>
2016-11-01 16:27:28 +1100

    WIP Travis-CI

--- Scripts/CI/Travis-CI/linux/install.sh
+++ Scripts/CI/Travis-CI/linux/install.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -ev
+
 sudo apt-get install -qq qt57base
 
 # source /opt/qt57/bin/qt57-env.sh
--- Scripts/CI/Travis-CI/linux/script.sh
+++ Scripts/CI/Travis-CI/linux/script.sh
@@ -2,11 +2,10 @@
 set -ev
 
 pwd
-# ls -l
 cd OverwatchPlayerLog
-# ls -l
+
 qmake OverwatchPlayerLog.pro -r "CONFIG+=debug"
-# ls -l
 make
+
 qmake OverwatchPlayerLog_UnitTest.pro -r
 make
