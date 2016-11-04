0924d0228003ee6d63a6b5d760f5b92b03329833
Benjamin Huang <y.x.ben.h+github@gmail.com>
2016-11-04 20:21:00 +1100

    fix appveyor build script won't exit with error on compile fail

--- Scripts/CI/AppVeyor/build_script.ps1
+++ Scripts/CI/AppVeyor/build_script.ps1
@@ -4,8 +4,14 @@ cd OverwatchPlayerLog
 
 # build DEBUG build.
 qmake OverwatchPlayerLog.pro "CONFIG+=debug"
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 mingw32-make
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 # build RELEASE build.
 qmake OverwatchPlayerLog.pro
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
 mingw32-make
+IF ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
