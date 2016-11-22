98153a1852a17c761d1bd1246a648d5f191cd3e6
Benjamin Huang <y.x.ben.h+github@gmail.com>
2016-11-22 19:59:25 +1100

    new Uuid helper class

---
+++ OverwatchPlayerLog-Android/app/src/main/java/me/benh/overwatchplayerlog/helpers/UuidHelper.java
@@ -0,0 +1,13 @@
+package me.benh.overwatchplayerlog.helpers;
+
+import java.util.UUID;
+
+/**
+ * Created by Benjamin Huang on 22/11/2016.
+ */
+
+public final class UuidHelper {
+    public static String NewUuidString() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
